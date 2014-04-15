2bf3464ed015cc5318ed7e6132809b8faef47240
Niels Dommerholt <niels@holmes.nl>
2014-04-15 15:41:23 +0200

    Initial commit

---
+++ src/main/java/com/nibado/zoo/listener/ZooListener.java
@@ -0,0 +1,10 @@
+package com.nibado.zoo.listener;
+
+public class ZooListener {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/nibado/zoo/service/ZooService.java
@@ -0,0 +1,10 @@
+package com.nibado.zoo.service;
+
+public class ZooService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
